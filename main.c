08ef0b46de122272aad1dc50c2f1c02fa65d4517
Dennis_Vargas <dennis.vargas@wsu.edu>
2018-02-25 15:16:43 -0800

    Created a ReadUserInput function. writes a command prompt "esh$>" and tests the ContinueInput() for an exit Prototypes for CleanInputNewLine() and ContinueInput(char*).
    Signed-off-by: Dennis_Vargas <dennis.vargas@wsu.edu>

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+
+#define INPUT_BUF 256
+
+typedef enum { false, true } bool;
+
+void ReadUserInput();
+char* CleanInputNewLine(char*);
+bool ContinueInput(char*);
+
 int main(int argc, char const *argv[])
 {
-    printf("Hello World\n");
+    ReadUserInput();
     return 0;
+}
+
+
+void ReadUserInput(){
+    char* user_input = malloc(sizeof(char)*INPUT_BUF);
+    do{
+        printf("esh$>");
+        fflush(stdin);
+        fgets(user_input, INPUT_BUF, stdin);
+        user_input = CleanInputNewLine(user_input);
+        ProcessUserInput();
+    }while(ContinueInput(user_input));
+    free(user_input);
 }
