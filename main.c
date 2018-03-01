8e3091f02072dab6be97d200abcfdf4b670b3022
Dennis_Vargas <dennis.vargas@wsu.edu>
2018-02-28 19:25:04 -0800

    Creates the part of ProcessInput() which tokenizes user input.
    User Input gets tokenized into an array where the first index is the program name and the rest of the array contains arguments.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <malloc.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define INPUT_BUF 256
 
@@ -12,13 +13,14 @@ char* CleanInputNewLine(char*);
 bool ContinueInput(char*);
 void ProcessUserInput(char*);
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
     RunEggShellPrompt();
     return 0;
 }
 
 char* CleanInputNewLine(char* user_input){
+//  Takes user input as argument checks if the final character is a newline character
+//  then changes that character to a NULL character == '\0'
     unsigned last_character = (unsigned)strlen(user_input)-1;
     if(user_input[last_character] == '\n')
         user_input[last_character] = '\0';
@@ -26,6 +28,8 @@ char* CleanInputNewLine(char* user_input){
 }
 
 bool ContinueInput(char* user_input){
+//  Takes user input as argumenent, tests
+//  to see if user input is equal to exit and returns false
     if (strcmp(user_input, "exit") == 0){
         return false;
     }else{
@@ -34,11 +38,32 @@ bool ContinueInput(char* user_input){
 };
 
 void ProcessUserInput(char* user_input){
-    char* args[] ={"/0"};
-    execv("ps",args);
+//    Takes user input as argument
+//    tokenizes the user input
+//    tokenized arguments are then added to an array named arguments
+//    The parent process forks and the child executes the arguments
+//
+    char* arguments [INPUT_BUF];
+    char *tok = user_input;
+    int arg_count = 0;
+    while((tok = strtok(tok, " ")) != NULL ){
+        arguments[arg_count] = tok;
+        tok = NULL;
+        arg_count++;
+    }
+    arguments[arg_count] = tok;
+    // once the argument is tokenized based on spaces
+    // fork the process and have the parent wait.
+    // child will run the command below.
+    if (execvp(arguments[0], arguments)< 0){
+        printf("Executable not Found Error: %s\n", strerror(errno));
+    }
 }
 
 void RunEggShellPrompt(){
+//    allocates memory to hold the user input.
+//    Then in a do while loop the shell prompt is printed,
+//
     char* user_input = malloc(sizeof(char)*INPUT_BUF);
     do{
         printf("esh$>");
